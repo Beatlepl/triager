6f3ae49d795084c2062ad098e58f86ad3aa8dffa
Bishwajeet Paul <bpaul@vmware.com>
2012-10-31 23:23:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triager::Application.config.secret_token = 'da3e817e7d412a2a33767c308ae64028183ba3b60780c2a2eda9fbd284cc303d9d95cca6fb466f1b9120832228133d56f45d32fb6d79d4646e796bd3e3280310'
